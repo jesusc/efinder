aeb72447b88d7e766b48c87e72d22662dcbd2d8b
Jesús Sánchez Cuadrado <jesus.sanchez.cuadrado@gmail.com>
2020-02-23 23:53:31 +0100

    Support two special usages of operators in EMF/OCL with a specific translation to USE

--- plugins/efinder.emfocl/src/efinder/emfocl/use/EMFOCL2UseFixer.java
+++ plugins/efinder.emfocl/src/efinder/emfocl/use/EMFOCL2UseFixer.java
@@ -8,12 +8,19 @@ import efinder.core.IDialectAdaptation;
 import efinder.core.ir.IRBuilder;
 import efinder.core.visitor.AbstractIRVisitor;
 import efinder.ir.CollectionTypeRef;
+import efinder.ir.EFPrimitiveType;
+import efinder.ir.MetaTypeRef;
 import efinder.ir.OrderedSetTypeRef;
 import efinder.ir.Specification;
 import efinder.ir.TypeRef;
 import efinder.ir.TypedElement;
+import efinder.ir.ocl.BooleanLiteralExp;
+import efinder.ir.ocl.IntegerLiteralExp;
+import efinder.ir.ocl.OclExpression;
 import efinder.ir.ocl.OclFactory;
 import efinder.ir.ocl.OperationCallExp;
+import efinder.ir.ocl.OperatorCallExp;
+import efinder.ir.ocl.OperatorKind;
 import efinder.ir.ocl.PropertyCallExp;
 import efinder.ir.ocl.SetLiteralExp;
 
@@ -27,14 +34,16 @@ public class EMFOCL2UseFixer extends AbstractIRVisitor<Void, Void> implements ID
 			if (o instanceof OperationCallExp) {
 				inOperationCallExp((OperationCallExp) o);
 			} else if (o instanceof PropertyCallExp) {
-				inPropertyCallExp((PropertyCallExp) o);			
+				inPropertyCallExp((PropertyCallExp) o);
+			} else if (o instanceof OperatorCallExp) {
+				inOperatorCallExp((OperatorCallExp) o);
 			//} else if (o instanceof TypedElement) {
 			//	inTypedElement((TypedElement) o);
 			} else if (o instanceof CollectionTypeRef) {
 				inCollectionTypeRef((CollectionTypeRef) o);
 			}
 		});
-	}
+	}	
 
 	// Change all OrderedSet by Set and let the FeatureChecker handle the invalid operations
 	private void inCollectionTypeRef(CollectionTypeRef ref) {
@@ -61,6 +70,43 @@ public class EMFOCL2UseFixer extends AbstractIRVisitor<Void, Void> implements ID
 		}
 	}
 
+	private void inOperatorCallExp(OperatorCallExp o) {
+		// Pattern: self.name.size() > 0
+		if (o.getOperator() == OperatorKind.GREATER || o.getOperator() == OperatorKind.GREATER_OR_EQUAL) {
+			if (o.getSource() instanceof OperationCallExp) {
+				OclExpression arg = o.getArgument();
+				if (arg instanceof IntegerLiteralExp && ((IntegerLiteralExp) arg).getValue() == 0) {					
+					OperationCallExp op = (OperationCallExp) o.getSource();
+					TypeRef type = op.getSource().getType();
+					if (op.getName().equals("size") && type instanceof MetaTypeRef && ((MetaTypeRef) type).getType() instanceof EFPrimitiveType) {
+						EFPrimitiveType pt = (EFPrimitiveType) ((MetaTypeRef) type).getType();
+						if (pt.getName().equals("String")) {
+							if (o.getOperator() == OperatorKind.GREATER) {
+								// Replace by self.name <> ''
+								OperatorCallExp replacement = IRBuilder.newOperatorCallExp(OperatorKind.DISTINCT, op.getSource(), IRBuilder.newStringExp(""));
+								EcoreUtil.replace(o, replacement);
+							} else {
+								// Replace by true because this is always true (the size of a string is always 0 or > 0)
+								BooleanLiteralExp replacement = IRBuilder.newBooleanExp(true);
+								EcoreUtil.replace(o, replacement);							
+							}
+							return;
+						}
+					}
+				}
+			}
+		}
+		
+		// Pattern: collection = single-value
+		if (o.getSource().getType() instanceof CollectionTypeRef && !(o.getArgument() instanceof CollectionTypeRef)) {
+			// These refers to ill-typed programs which are considered valid by EMF/OCL
+			// The result would always be false
+			BooleanLiteralExp replacement = IRBuilder.newBooleanExp(false);
+			EcoreUtil.replace(o, replacement);	
+		}
+		
+	}
+	
 	// Report the changes?
 	public void inOperationCallExp(@NonNull OperationCallExp self) {
 		if ( self.getName().equals("oclAsSet") ) {			
